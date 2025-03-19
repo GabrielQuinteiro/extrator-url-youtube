dcac3710ac2f9ab258d473defbaa27c4333b5f85
Gabriel Carvalho <gabrieldecb@gmail.com>
2025-03-19 14:32:47 -0300

    Alterando estilos da extensão

--- content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "getVideoURL") {
-        let iframe = document.querySelector("iframe[src*='youtube.com/embed/']");
-        sendResponse({ url: iframe ? iframe.src : null });
-    }
-});
